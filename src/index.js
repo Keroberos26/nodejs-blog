df1e6d52c81290d63e6f1ce4e806dad3e27d6e93
Hieu Tran <davidtranit110@gmail.com>
2023-10-28 21:30:44 +0700

    Apply MVC

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ const morgan = require('morgan');
 const { engine } = require('express-handlebars');
 
 const app = express();
-const port = 3000;
+const port = 4000;
+
+const route = require('./routes');
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')))
-
-console.log(path.join(__dirname, 'public'))
 
 // HTTP Logger
 app.use(morgan('combined'));
@@ -21,13 +21,6 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  res.render('home');
-});
-
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+route(app);
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
