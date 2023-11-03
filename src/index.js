c069949b5e1c37ac76589023cea985b8224ff804
Hieu Tran <davidtranit110@gmail.com>
2023-11-03 23:11:47 +0700

    Prettier

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const { engine } = require('express-handlebars');
+const {engine} = require('express-handlebars');
 
 const app = express();
 const port = 4000;
@@ -9,15 +9,18 @@ const port = 4000;
 const route = require('./routes');
 
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // HTTP Logger
 app.use(morgan('combined'));
 
 //Template engine
-app.engine('hbs', engine({
-  extname: '.hbs',
-}));
+app.engine(
+  'hbs',
+  engine({
+    extname: '.hbs',
+  }),
+);
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
