1d7dbc7c0cb64c49e82e24557ed0196cd257f481
Hieu Tran <davidtranit110@gmail.com>
2023-11-07 23:19:59 +0700

    Course List and Detail Page

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,15 +1,12 @@
 const Course = require('../models/Course');
+const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class SiteController {
   // [GET] /
-  async index(req, res) {
-    // res.render('home');
-    try {
-      const course = await Course.find({});
-      res.json(course);
-    } catch {
-      res.status(400).json({ err: 'ERROR!!!' });
-    }
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) => res.render('home', { courses: multipleMongooseToObject(courses) }))
+      .catch(next);
   }
 
   // [GET] /search
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 const newsRouter = require('./news.route');
 const siteRouter = require('./site.route');
+const courseRouter = require('./courses.route');
 
 function route(app) {
   app.use('/news', newsRouter);
-
+  app.use('/courses', courseRouter);
   app.use('/', siteRouter);
 }
 
