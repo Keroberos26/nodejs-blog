166a2fa8c90ba55991854838655b39ab208bcc52
Keroberos <jinnguyen054@gmail.com>
2023-11-18 14:09:00 +0700

    Checkbox All

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -2,14 +2,19 @@ const Course = require('../models/Course');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 
 class MeController {
-  //[GET] /stored/courses
+  //[GET] /me/stored/courses
   storedCourses(req, res, next) {
-    Course.find({})
-      .then((courses) => res.render('me/stored-courses', { courses: multipleMongooseToObject(courses) }))
+    Promise.all([Course.find({}), Course.countDocumentsWithDeleted({ deleted: true })])
+      .then(([courses, deletedCount]) =>
+        res.render('me/stored-courses', {
+          deletedCount,
+          courses: multipleMongooseToObject(courses),
+        }),
+      )
       .catch(next);
   }
 
-  //[GET] /trash/courses
+  //[GET] /me/trash/courses
   trashCourses(req, res, next) {
     Course.findWithDeleted({ deleted: true })
       .then((courses) => res.render('me/trash-courses', { courses: multipleMongooseToObject(courses) }))
